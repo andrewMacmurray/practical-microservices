c51d9bfcbdf425c6a7dfaf0f07d6b7d29ab9bf05
andrewMacmurray <a.macmurray@icloud.com>
2021-04-25 18:29:06 +0100

    remove scheduling

--- src/main/kotlin/io/videos/application/Application.kt
+++ src/main/kotlin/io/videos/application/Application.kt
@@ -2,12 +2,10 @@ package io.videos.application
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.scheduling.annotation.EnableScheduling
 
-@EnableScheduling
 @SpringBootApplication
 class Application
 
 fun main(args: Array<String>) {
-	runApplication<Application>(*args)
+    runApplication<Application>(*args)
 }
